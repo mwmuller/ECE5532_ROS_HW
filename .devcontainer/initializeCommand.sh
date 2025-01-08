aafef98d1cd2a86a72d31a5d3764b3ecc89d7e37
mwmuller <mwmuller94@gmail.com>
2025-01-08 00:02:15 -0500

    Adding scripts and updated dockerfile

--- .devcontainer/initializeCommand.sh
+++ .devcontainer/initializeCommand.sh
@@ -6,11 +6,14 @@ ROOT_DIR_PATH=$(git rev-parse --show-toplevel)
 USER=$(whoami)
 
 DEV_CONTAINER_FOLDER="${ROOT_DIR_PATH}/.devcontainer"
-export DOCKER_IMAGE_NAME="ece5532_ros_${localEnv:USER}"
+BASE_IMAGE="darknessoup/ros-noetic-20.04:20250107_1"
+
+export DOCKER_IMAGE_NAME="ece5532_ros_${USER}"
 docker build -f ${DEV_CONTAINER_FOLDER}/Dockerfile \
     --build-arg DOCKER_IMAGE="${DOCKER_IMAGE}" \
     --build-arg USERNAME="${USER}" \
     --build-arg USER_UID="$(id -u)" \
     --build-arg USER_GID="$(id -g)" \
+    --build-arg BASE_IMAGE="${BASE_IMAGE}" \
     -t ${DOCKER_IMAGE_NAME} .
 
